8a4b6cf751a576ccd0b3bed80a0a5ab5f8cdeb9d
SAI PRAVEEN KORUBILLI <saipraveensuperj@gmail.com>
2026-03-24 12:45:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (72.47%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        result = []
+        def backtrack(open_count, close_count, current):
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+            if open_count < n:
+                backtrack(open_count + 1, close_count, current + '(')
+            if close_count < open_count:
+                backtrack(open_count, close_count + 1, current + ')')
+        backtrack(0, 0, '')
+        return result
